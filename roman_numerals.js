0d0fa74189fe9f6e9ea79d30354a32894cdc26c5
Matthew Culliford <matthew.culliford@dvsa.gov.uk>
2022-07-29 10:00:27 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:roman_numerals.js

--- roman_numerals.js
+++ roman_numerals.js
@@ -2,7 +2,7 @@
 function numberToNumerals (numberInput){
     let convertedNumber = '';
 
-    const roman = {1:'I',2:'II',3:'III',4:'IV',5:'V'}
+    const roman = {1:'I',2:'II',3:'III',4:'IV',5:'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX', 10: 'X' }
 
     convertedNumber = roman[numberInput];
 
