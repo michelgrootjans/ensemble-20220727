1a9e2a6098fc7eac66f730b0edaed12f1ed669b1
Michel Grootjans <michel.grootjans@gmail.com>
2022-08-02 08:18:36 +0200

    bowling game implemented by team 1

---
+++ bowling.test.js
@@ -0,0 +1,33 @@
+const Game = require("./bowling");
+
+fdescribe('bowling', () => {
+    test('roll 0', () => {
+        const game = new Game();
+        expect(game.score).toEqual(0);
+    });
+    test('roll 3', () => {
+        const game = new Game();
+        game.roll(1);
+        game.roll(2);
+        expect(game.score).toEqual(3);
+    });
+    test('roll 5', () => {
+        const game = new Game();
+        game.roll(1);
+        game.roll(4);
+        expect(game.score).toEqual(5);
+    });
+    test('roll mutliple frames', () => {
+        const game = new Game();
+        game.roll(1);
+        game.roll(4);
+        game.roll(6);
+        expect(game.score).toEqual(11);
+    });
+    test('strike', () => {
+       const game = new Game();
+       game.roll(10);
+       game.roll(5);
+       expect(game.score).toEqual(20);
+    });
+})
